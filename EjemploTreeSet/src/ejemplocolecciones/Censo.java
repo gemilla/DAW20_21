d8bdcca801bcd45603e66110d19e29af9527c641
gema <gema@192.168.56.1>
2021-05-25 10:47:51 +0200

    Ejemplo TreeSet

---
+++ EjemploTreeSet/src/ejemplocolecciones/Censo.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplocolecciones;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author GEMA
+ */
+public class Censo implements Serializable {
+    //inserción ordenada y sin duplicados teniendo en cuenta el compareTo de Individuo, orden natural
+    protected TreeSet<Individuo> censo;
+
+    public Censo(TreeSet<Individuo> censo) {
+        this.censo = censo;
+    }
+
+    public Censo() {
+        //primera ejecución
+        censo = new TreeSet();
+        for (int i = 0; i < 30; i++) {
+            censo.add(new Individuo((int) (Math.random() * 100), "individuo" + (i + 1), "provincia" + (i % 5)));
+        }
+
+        //sucesivas, carga de fichero
+        //censo =GestorFicheros.leerFicheroBinario(GestorFicheros.FICHERO_BINARIO).censo;
+    }
+
+    public TreeSet<Individuo> getCenso() {
+        return censo;
+    }
+
+    public void setCenso(TreeSet<Individuo> censo) {
+        this.censo = censo;
+    }
+
+    public void listar() {
+        Iterator<Individuo> aux = censo.iterator();
+        while(aux.hasNext()){
+            System.out.println(aux.next());
+        }
+    }
+
+    public void alta() throws IOException {
+        censo.add(Alta());
+    }
+
+    public static Individuo Alta() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduce edad, nombre,poblacion");
+        return new Individuo(Integer.parseInt(br.readLine()), br.readLine(), br.readLine());
+
+    }
+
+    public void baja(String poblacion) throws IOException {
+        Iterator<Individuo> it = censo.iterator();
+        while (it.hasNext()) {
+            Individuo aux = it.next();
+            if (aux.getPoblacion().equalsIgnoreCase(poblacion)) {
+                it.remove();
+            }
+        }
+    }
+
+    public void bajaArrayList(String poblacion) throws IOException {
+        ArrayList<Individuo> lista = new ArrayList(censo);
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).poblacion.equalsIgnoreCase(poblacion)) {
+                lista.remove(i);
+                i--;
+            }
+
+        }
+    }
+    public void mostrar(String nombre) {
+        Iterator<Individuo> it = censo.iterator();
+        while (it.hasNext()) {
+            Individuo aux = it.next();
+            if (aux.getNombre().equalsIgnoreCase(nombre)) {
+                System.out.println(aux.toString());
+            }
+        }
+    }
+
+    public void mostrarPoblacion(String poblacion) {
+        /**/
+    }
+
+    public void mostrarMayores(int edad) {
+        /**/
+    }
+
+    public void actualizarPoblacion(String nombre, String poblacion) throws IOException {
+        Iterator<Individuo> it = censo.iterator();
+        while (it.hasNext()) {
+            Individuo aux = it.next();
+            if (aux.getNombre().equalsIgnoreCase(nombre)) {
+                aux.poblacion = poblacion;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return censo.toString();
+    }
+
+}
---
+++ EjemploTreeSet/src/ejemplocolecciones/Individuo.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplocolecciones;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author GEMA
+ */
+public class Individuo implements Serializable, Comparable<Individuo>{
+    protected int edad;
+    protected String nombre;
+    protected String poblacion;
+
+    public Individuo(int edad, String nombre, String poblacion) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%15s %4d %15s\n",nombre, edad, poblacion);
+    }
+
+     public String toStringFichero() {
+        return edad + ";" + nombre + ";" + poblacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.edad;
+        hash = 29 * hash + Objects.hashCode(this.nombre);
+        hash = 29 * hash + Objects.hashCode(this.poblacion);
+        return hash;
+    }
+
+    
+
+    @Override
+    public boolean equals (Object o){
+        boolean iguales= false;
+        if (this.edad ==((Individuo)o).edad && this.nombre.equals(((Individuo)o).nombre)&& this.poblacion.equals(((Individuo)o).poblacion))
+           iguales=true; 
+        return iguales;
+    }
+
+    @Override
+    public int compareTo(Individuo t) {
+        return this.nombre.compareTo(t.nombre);
+    }
+
+   
+     
+    
+}
